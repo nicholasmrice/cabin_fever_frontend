e42d035decc3d57e210f2416de8f181b01e5dafd
Your Name <you@example.com>
2021-01-31 10:46:29 -0700

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 // Components
 import Home from './pages/cabins/home.js'
@@ -7,57 +6,6 @@ import ShowCabin from './pages/cabins/show.js'
 import NavBar from './components/nav_bar.js'
 
 class App extends React.Component {
-    state = {
-        address: '',
-        description: '',
-        cabins: []
-    }
-    handleChange = (event) => {
-        this.setState({
-            [event.target.id]: event.target.value
-        })
-    }
-    // Create
-    createCabin = (event) => {
-        event.preventDefault()
-        axios
-            .post('/cabins', this.state)
-            .then((res) => {this.getCabins()})
-    }
-    // Read
-    getCabins = () => {
-        axios
-            .get('/cabins')
-            .then((res) => {
-                this.setState({
-                    cabins: res.data,
-                    address: '',
-                    description: ''
-                })
-            })
-            .catch((err) => {console.log(err)})
-    }
-    // Update
-    updateCabin = (event) => {
-        event.preventDefault()
-        event.target.reset()
-        axios
-            .put('/cabins/' + event.target.id, this.state)
-            .then((res) => {
-                this.getCabins()
-            })
-    }
-    // Delete
-    deleteCabin = (event) => {
-        axios
-        .delete('/cabins/' + event.target.value)
-        .then((res) => {
-            this.getCabins()
-        })
-    }
-    componentDidMount = () => {
-        this.getCabins()
-    }
     render = () => {
         return (
             <Router>
